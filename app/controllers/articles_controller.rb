669085a8d997d834f7655f908d9a68cc68a65d6a
shri <shrija.tumma1994@gmail.com>
2020-05-13 21:55:41 +0530

    Setup user sign in

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
 class ArticlesController < ApplicationController
+  # before_action :authenticate_user!, expect: [:index, :show]
   before_action :set_article, only: [:show, :edit, :update, :destroy]
+  
   def index
     @articles = Article.all
   end
